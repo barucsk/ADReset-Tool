3762797546f62f83608f1d29732eec10ecf2861a
barucsk <60474300+barucsk@users.noreply.github.com>
2022-08-27 15:07:42 -0500

    Add files via upload

---
+++ resetTool.ps1
@@ -0,0 +1,25 @@
+﻿while(0 -lt 1)
+{
+    $user = Read-Host "Enter username"
+    #Save the object and show the result that we want, 
+    #[10] = Password last set
+    #[11] = Password expires
+    # [7] = Account active
+    $userData = net user $user /domain
+    Write-Host $userData[10]
+    Write-Host $userData[11]
+    Write-Host $userData[7]
+    if($userData[7] -like "*Locked")
+    {
+        $temp = net user $user /domain /active:yes
+        Write-Host "The account has been unlocked successfully"
+    }
+    $reset = Read-Host "Do you want to reset the password? Press Y or N"
+    if($reset -eq "y")
+    {
+        $pass = Read-Host "Enter new password"
+        $userData = net user $user $pass /domain
+        Write-Host "Password changed successfully"
+    }
+    Write-Host "***************************************************"
+}
